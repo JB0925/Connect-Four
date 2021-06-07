8f1c25c964576d927da851b1fd8202e6f930dd7b
Jesse Brink <jbrink0925@gmail.com>
2021-06-06 22:50:16 -0400

    fix tests; remove extra calls to makeHtmlBoard and makeBoard

--- connect4.test.js
+++ connect4.test.js
@@ -3,31 +3,14 @@ describe('create the in-memory game board', () => {
         expect(board.length).toEqual(6);
         expect(board[0].length).toEqual(7);
     });
-
-    afterEach(() => {
-        board = [];
-    })
 });
 
 describe('create the html board', () => {
     let gameBoard = document.querySelector('#board');
-
-    afterEach(() => {
-        gameBoard.innerHTML = '';
-    });
     
     it('should create a game board in the DOM with 7 child elements', () => {
-        makeHtmlBoard();
         expect(gameBoard.childElementCount).toBe(7);
     });
-
-    it('should create a game board with 14 child elements', () => {
-        let WIDTH = 10;
-        let HEIGHT = 5;
-        makeHtmlBoard();
-        expect(gameBoard.childElementCount).toEqual(14);
-    })
-
 });
 
 describe('return the name of the appropriate CSS class', () => {
@@ -35,27 +18,25 @@ describe('return the name of the appropriate CSS class', () => {
         expect(addClass(null)).toEqual('zero');
         expect(addClass(3)).toEqual('three');
         expect(addClass(10)).toEqual(undefined);
+        expect(addClass(4)).toEqual('four');
+        expect(addClass(1)).toEqual('one');
+        expect(addClass(5)).toEqual('five');
+        expect(addClass(2)).toEqual('two');
     });
 });
 
 describe('return the lowest available row to place a chip in', () => {
     let gameBoard = document.querySelector('#board');
 
-    afterEach(() => {
-        gameBoard.innerHTML = '';
-    });
-
-    it('should return a number 0-5 or null', () => {
-        makeBoard();
-        makeHtmlBoard();
+    it('should return 4', () => {
         let allTds = document.querySelectorAll('td')
         for (let td of allTds) {
             let id = td.id
             if (id === '0-1' || id === '0-0') {
                 let testDiv = document.createElement('div');
                 td.append(testDiv);
-            }
-        }
+            };
+        };
         expect(findSpotForCol(0)).toEqual(4);
     });
 });
